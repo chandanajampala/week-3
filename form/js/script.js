3e44f55ed5394d41e6926029b4e1dbee5532494f
Chandana Jampala <cjampala@virtusa.com>
2018-06-18 17:36:44 +0530

    form

--- form/js/script.js
+++ form/js/script.js
@@ -1,146 +1,215 @@
-$(document).ready(function(){
-    var date_input=$('input[name="date"]'); //our date input has the name "date"
-    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-    date_input.datepicker({
-        format: 'mm/dd/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-    })
-})
+var valid = 0;
+$(function(){
+    $("span").hide();
 
-
-//$('.dropify').dropify();
-
-
-function onSubmit(){
-       if(validateFirstName()){
-           if(validateFullName()){
-               if(validateDesignation()){
-                   if(validateEmpCode()){
-                       if(validateBloodGrp()){
-                           if(validateEmail()){
-                               if(validateNumber()){
-                                   if(validateEmerg()){
-                                       postData();
-                                   }
-                                   else{
-                                       //throw error num
-                                   }
-                               }
-                               else{
-                                   //throw error num
-                               }
-                           }
-                           else{
-                               // error for email
-                           }
-                       }
-                       else{
-                           //throe error for blood grp
-                       }
-                   }
-                   else{
-                       //error for emp code
-
-                   }
-               }
-               else{
-                   //error for designation
-               }
-           }
-           else{
-               //error for full name
-           }
-
-       }
-       else{
-           //errorfor name
-       } 
-
-
-}
-
-function validateFirstName(){
-    var name = $('#firstName').val();
-   return checkAlpha(name);
-}
-
-
-function validateFullName(){
-    var name = $('#fullName').val();
-   return checkAlpha(name);
-}
-
-function validateFirstName(){
-    var name = $('#desig').val();
-   return checkAlpha(name);
-}
-
-function validateEmpCode()
+});
+function addDetails()
 {
-    var code=$('#empCode').val();
-    return checkNum(code,7);
-}
+    valid = 0;
+    $("span").hide();
 
-
-function validateNumber()
-{
-    var num=$('#empNo').val();
-    return checkNum(num,10);
-}
-
-function validateEmerg()
-{
-    var num=$('#emergencyNo').val();
-    return checkNum(num,10);
-}
-
-function validateBloodGrp(){
-    var grp=$('#bloodGroup')
-    var blood1Pattern =  /(A|B|AB|O)[+-]/;
-  if(grp.val.match(blood1Pattern)){
-      return true;
-  }  
-  else{
-      return false;
-  }
-}
-
-function validateEmail(){
-    var email=$('#email').val();
-    if (/@virtusa\.com$/.test(email)) {
-        return true;
-    }
-    else{
-        return false;
+    var validate = validateDetails();
+    if(validate)
+    {
+        postTheData();
     }
     
 }
-
-
-function checkAlpha(name){
-    var letters = /^[A-Za-z]+$/;
-    if(name.value.match(letters)&&name.value.length!=0)
-      {
-       return true;
+function validateDetails()
+{
+    validateFirstName();
+    validateFullName();
+    validateDesignation();
+    validateEmployeeNumber();    
+    validateReasonsforIssues();    
+    validateEmployeeMobileNumber();
+    validateEmployeeEmergencyNumber();
+    validateEmailId();
+    validateBloodGroup();
+    if(valid == 1)
+    {
+        return false;
+    }
+    return true;
+}
+function validateFirstName()
+{
+   if(checkNull("firstName"))
+   {
+        valid = 1;
+        showError("errorMessageForFirstName", "firstName");
+   }
+  else{
+   $('#firstName').css("border","solid 1px green");
+}}
+function validateFullName()
+{
+   if(checkNull("fullName"))
+   {
+       valid = 1;
+       showError("errorMessageForFullName", "fullName");
+      
+   }
+   else{
+    $('#fullName').css("border","solid 1px green");
+ }
+}
+function validateDesignation()
+{
+    if(checkNull("desig"))
+   {
+       valid = 1;
+       showError("errorMessageForDesignation", "desig");
+      
+   } else{
+    $('#designation').css("border","solid 1px green");
+ }
+}
+function validateEmployeeNumber()
+{
+    if(checkNull("empCode"))
+   {
+       valid = 1;
+       showError("errorMessageForEmployeeNumber", "empCode");
+      
+   }
+     else if(checkNumbersOnly("emp_no")) {
+        valid = 1;
+        showError("errorMessageForEmployeeNumber", "empCode");
+    
       }
-    else
-      {
-            return false;
+      else if(checkLength("emp_no", 7)) {
+        valid = 1;
+        showError("errorMessageForEmployeeNumber", "empCode");
+    
       }
- 
+      else{
+        $('#emp_no').css("border","solid 1px green");
+     }
 }
 
-function checkNum(num ,len)
+
+function validateEmailId()
 {
-   var numbers = /^[0-9]+$/;
-   if(num.value.match(numbers)&&num.value.length==len)
+    if(checkNull("email")||checkEmail("email"))
    {
-    return true;
+       valid = 1;
+       showError("errorMessageEmailID", "email");
+      
    }
-   else
+   else{
+    $('#email').css("border","solid 1px green");
+ }
+//  if(checkEmail("email")) {
+//     valid = 1;
+//     showError("errorMessageEmailID", "email");
+       
+//    }
+}
+
+function validateEmployeeMobileNumber()
+{
+    if(checkNull("empNo"))
    {
-      return false;
+       valid = 1;
+       showError("errorMessageForEmployeeMobileNumber", "empNo");
+        
    }
-} 
+     else if(checkNumbersOnly("empNo")) {
+        valid = 1;
+        showError("errorMessageForEmployeeMobileNumber", "empNo");
+    
+      }
+      else{
+        $('#mobile').css("border","solid 1px green");
+     }
+
+}
+function validateEmployeeEmergencyNumber()
+{
+    if(checkLength("emergencyNo", 10)) 
+    {
+        valid = 1;
+        showError("errorMessageForEmployeeEmergencyContactNumber", "emergencyNo");
+    
+      }
+      else{
+        $('#emergency').css("border","solid 1px green");
+     }
+}
+
+function checkNull(id)
+{
+    const value = $(`#${id}`).val();
+    if(value == null || value == '')
+    {
+        return true;
+    }
+    return false;
+}
+function checkNumbersOnly(id)
+{
+    const pattern=/^[1-9][0-9]{9}/;
+    const value=$(`#${id}`).val();
+    const result=value.match(pattern);
+    if(result==null)
+    {
+        return true;
+    }
+    return false;
+    
+    
+}
+function checkLength(id, length)
+{
+    const value=$(`#${id}`).val();
+    if(value.length != length)
+    {
+        return true;
+    }
+    return false;
+}
+function checkEmail(id)
+  
+   {  const value=$(`#${id}`).val();
+    const valid=/^[a-z|A-Z][a-z|A-Z|0-9|]+@virtusa.com/;
+    // "/^([A-Za-z0-9_\-\.])+\@[virtusa.com]$/";
+    const result=value.match(valid);
+    console.log(result);
+    if(result==null)
+    {
+        return true;
+    }
+    return false;
+   }
+
+function showError(spanId, inputId)
+{
+    $(`#${spanId}`).show();
+    $(`#${inputId}`).addClass('error');
+}
+
+function postTheData()
+{
+    console.log("Finally inside posting the data");
+    const firstName = $("#firstName").val();
+    const fullName = $("#fullName").val();
+    const formDetails = {
+        "firstName" : firstName,
+        "fullName" : fullName
+    }
+    const data = JSON.stringify(formDetails);
+   $.ajax({
+       type: "POST",
+       url: "https://application-form-fd16d.firebaseio.com/ApplicationIds.json",
+        data: data,
+       success : function(data)
+       {
+          console.log("In success");
+      },
+        error : function(err)
+        {
+            console.log(err);
+        }
+      });
+}
